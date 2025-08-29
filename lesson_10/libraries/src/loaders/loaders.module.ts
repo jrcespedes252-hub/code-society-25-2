baea7c3e81bcf5f59cdac8d6d47777a8b343833b
jrcespedes252-hub <jrcespedes252@gmail.com>
2025-08-29 15:39:22 +0000

    feat: lesson 10 add JaizelcespedesLoader with CSV data loading

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { JaizelcespedesLoader } from './jaizelcespedes_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+  JaizelcespedesLoader,
 ];
 
 @Module({
