a1d7ac17e94c31bcca5faa0b28d1e6f4aad35124
jrcespedes252-hub <jrcespedes252@gmail.com>
2025-09-05 15:06:32 +0000

    feat: implement findPermutationDifference for LeetCode 3146

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,8 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +10,23 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    // Create a map to store the index of each character in string t
+    Map<Character, Integer> indexMap = new HashMap<>();
+
+    // Fill the map with characters and their indices from string t
+    for (int i = 0; i < t.length(); i++) {
+      indexMap.put(t.charAt(i), i);
+    }
+
+    int totalDifference = 0;
+
+    // For each character in string s, find its index in t and calculate the difference
+    for (int i = 0; i < s.length(); i++) {
+      char currentChar = s.charAt(i);
+      int indexInT = indexMap.get(currentChar);
+      totalDifference += Math.abs(i - indexInT);
+    }
+
+    return totalDifference;
   }
 }
